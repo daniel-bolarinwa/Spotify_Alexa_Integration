2eb2f85d8dbe0f141eafb72191b74ef540d74633
Daniel Bolarinwa <dtireni@gmaail.com>
2022-05-16 23:24:10 +0100

    authentication and authorisation decoupling via django web app

--- spotify-service/alexa_spotify_app/spotify_client.py
+++ spotify-service/alexa_spotify_app/spotify_client.py
@@ -4,12 +4,12 @@ class SpotifyClient(object):
     def __init__(self, api_token):
         self.api_token = api_token
 
-    def get_current_song(self, api_token):
+    def get_current_song(self):
         query = "https://api.spotify.com/v1/me/player/currently-playing?market=GB"
         response = requests.get(
             query,
             headers={
-                "Authorization": f"Bearer {api_token}"
+                "Authorization": f"Bearer {self.api_token}"
             }
         )
         response_json = response.json()
@@ -29,13 +29,13 @@ class SpotifyClient(object):
 
         return current_track_info
 
-    def search_song(self, artist, track, api_token):
+    def search_song(self, artist, track):
         query = f"https://api.spotify.com/v1/search?q=track%3A{track}+artist%3A{artist}&type=track&offset=0&limit=20"
         response = requests.get(
             query,
              headers={
                 "Content-Type": "application/json",
-                "Authorization": f"Bearer {api_token}"
+                "Authorization": f"Bearer {self.api_token}"
              }
         )
         response_json = response.json()
@@ -45,7 +45,7 @@ class SpotifyClient(object):
         else:
             raise Exception(f"No song found for {artist} = {track}")
 
-    def add_song_to_spotify(self, song_id, api_token):
+    def add_song_to_spotify(self, song_id):
         url = "https://api.spotify.com/v1/me/tracks"
         response = requests.put(
             url,
@@ -54,7 +54,7 @@ class SpotifyClient(object):
             },
             headers={
                 "Content-Type": "application/json",
-                "Authorization": f"Bearer {api_token}"
+                "Authorization": f"Bearer {self.api_token}"
             }
         )
         return response.ok
